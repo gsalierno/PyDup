58d31cee1e4a34f855d3f1b1e61477aa6b14b18d
giuliosalierno <salierno.g92@gmail.com>
2019-12-02 16:58:16 +0100

    Added analysis of questions with word2vec and k-means for clustering

--- Analyze.py
+++ Analyze.py
@@ -1,8 +1,11 @@
-from Parser import Parser
+import re
+from gensim.models import Word2Vec
+from nltk.cluster import KMeansClusterer
+import nltk
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-#p = Parser('../OSBDD_bckp_2.docx')
-#p.print_dict()
-#p.writeDictToFile()
 
 def readDictFromFile(path):
     dict = {}
@@ -16,26 +19,72 @@ def readDictFromFile(path):
 
 
 def cleanAndSplitAnswer(a):
-    a = a.replace('[',"")
-    a = a.replace("]","")
+    a = a.replace('[', "")
+    a = a.replace("]", "")
     if a.startswith("'") and a.endswith("'"):
         a = a[1:-1]
     return a
 
 
-def cleanAnsw(dict_quest):
-    for k in questions.keys():
-        answ_list = questions[k]
-        answ = answ_list[len(answ_list) - 1] #get last answ with \n
-        answ.strip('\n')
-
-def duplicate_count(s):
-    return len([x for x in set(s) if s.count(x) > 1])
-
 def printDictionary(questions):
     for k in questions.keys():
-        print("QUESTION: ",k, "ANSWERS: ", questions[k])
+        print("QUESTION: ", k, "ANSWERS: ", questions[k])
+
+
+def sent_vectorizer(sent, model):
+    sent_vec = []
+    numw = 0
+    for w in sent:
+        try:
+            if numw == 0:
+                sent_vec = model[w]
+            else:
+                sent_vec = np.add(sent_vec, model[w])
+            numw += 1
+        except:
+            pass
+
+    return np.asarray(sent_vec) / numw
+
+
+def clean_quest(questions):
+    tokenizer = nltk.data.load('tokenizers/punkt/italian.pickle');
+    sent = []
+    for q in questions:
+        # Normalize tabs and remove newlines
+        no_tabs = str(q).replace('\t', ' ').replace('\n', '')
+        # Remove all characters except A-Z and a dot.
+        alphas_only = re.sub("[^a-zA-Z\.]", " ", no_tabs);
+        # Normalize spaces to 1
+        multi_spaces = re.sub(" +", " ", alphas_only);
+        # Strip trailing and leading spaces
+        no_spaces = multi_spaces.strip();
+        # Normalize all charachters to lowercase
+        clean_text = no_spaces.lower();
+        # Get sentences from the NLTK tokenizer, remove the dot in each.
+        sentences = word_tokenize(clean_text)
+        sentences = [re.sub("[\.]", "", sentence) for sentence in sentences]
+        sent.append(sentences)
+    return sent
+
 
 questions = readDictFromFile('../questions_raw_cleaned.txt')
 
-printDictionary(questions)
+# CLUSTER ANALYSIS WITH WORD EMBEDDINGS AT SENTENCE LAYER
+
+# PREPROCESSING
+sentences = clean_quest(list(questions.keys()))
+# TRAIN WORD2VEC MODEL
+model = Word2Vec(sentences, min_count=2)
+X = []
+for sentence in sentences:
+    X.append(sent_vectorizer(sentence, model))
+print("========================")
+# CLUSTERING WITH K-MEANS
+NUM_CLUSTERS = 5
+kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=nltk.cluster.util.cosine_distance)
+assigned_clusters = kclusterer.cluster(X, assign_clusters=True)
+print(assigned_clusters)
+
+for index, sentence in enumerate(sentences):
+    print(str(assigned_clusters[index]) + ":" + str(sentence))
